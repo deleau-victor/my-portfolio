ff4dd1916a13a5e20ad6fa591ab889cebc082fc3
deleau-victor <deleau.victor.pro@gmail.com>
2023-01-10 02:19:53 +0100

    updated filters.ts

--- filters.ts
+++ filters.ts
@@ -6,15 +6,19 @@ export interface IFilters {
 
 const filters: IFilters[] = [
 	{
-		name: "TypeScript",
-		icon: "/images/icons/typescript.svg",
+		name: 'TypeScript',
+		icon: '/images/icons/typescript.svg',
 		id: 1,
 	},
-	{ name: "React", icon: "/images/icons/react.svg", id: 2 },
-	{ name: "Next", icon: "/images/icons/nextjs-square.svg", id: 3 },
-	{ name: "NodeJS", icon: "/images/icons/nodejs-icon.svg", id: 4 },
-	{ name: "React Native", icon: "/images/icons/react.svg", id: 5 },
-	{ name: "Redux", icon: "/images/icons/redux.svg", id: 6 },
+	{ name: 'React', icon: '/images/icons/Frameworks Front/react.svg', id: 2 },
+	{ name: 'Next', icon: '/images/icons/Frameworks Front/nextjs.svg', id: 3 },
+	{ name: 'NodeJS', icon: '/images/icons/Dev Skills/node.svg', id: 4 },
+	{
+		name: 'React Native',
+		icon: '/images/icons/Frameworks Front/react.svg',
+		id: 5,
+	},
+	{ name: 'Redux', icon: '/images/icons/Frameworks Front/redux.svg', id: 6 },
 ]
 
 export default filters
