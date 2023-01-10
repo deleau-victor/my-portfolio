21b47a7c5c657204a296a3753736bdb9dd76fa92
deleau-victor <deleau.victor.pro@gmail.com>
2023-01-10 11:51:36 +0100

    ajustements css

--- components/sections/projects/filtersSection.tsx
+++ components/sections/projects/filtersSection.tsx
@@ -1,6 +1,7 @@
-import React, { FC } from "react"
-import { IFilters } from "../../../filters"
-import Icon from "../../UI/icon"
+import React, { FC, useState } from 'react'
+import { IFilters } from '../../../filters'
+import Button from '../../UI/button'
+import Icon from '../../UI/icon'
 
 type FiltersProps = {
 	filterList: IFilters[]
@@ -13,19 +14,32 @@ const FiltersSection: FC<FiltersProps> = ({
 	filterList,
 	isSelected,
 }) => {
+	const [isFilterVisible, setIsFilterVisible] = useState(false)
+
 	return (
-		<section>
-			{filterList.map(({ name, icon, id }) => {
-				return (
-					<div
-						key={id}
-						onClick={() => onClick(id)}
-						className={isSelected(id) ? "selected" : ""}>
-						<Icon src={icon} alt={`logo ${name}`} responsive />
-						<p>{name}</p>
-					</div>
-				)
-			})}
+		<section className={isFilterVisible ? 'filter' : ''}>
+			<Button
+				onClick={() => setIsFilterVisible(!isFilterVisible)}
+				variant='outlined'
+				borderColor='border-white'
+				size='xs'
+			>
+				Filtres
+			</Button>
+			<div>
+				{filterList.map(({ name, icon, id }) => {
+					return (
+						<div
+							key={id}
+							onClick={() => onClick(id)}
+							className={isSelected(id) ? 'selected' : ''}
+						>
+							<Icon src={icon} alt={`logo ${name}`} responsive />
+							<p>{name}</p>
+						</div>
+					)
+				})}
+			</div>
 		</section>
 	)
 }
